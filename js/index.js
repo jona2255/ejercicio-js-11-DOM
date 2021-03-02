7414d0287d93891630148eacd45a2bd01be76302
Carlos Solano <carlos.solanogomez@hotmail.com>
2021-03-02 20:03:36 +0100

    primera letra mayuscula

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 const listaNodos = document.querySelectorAll(".palabra");
+copiarNodo();
+function copiarNodo() {
+  for (const nodo of listaNodos) {
+    nodo.addEventListener("click", () => {
+      console.log(nodo);
+      const nodoCopiar = nodo.cloneNode(true);
+      const resultados = document.querySelector(".resultado");
+      resultados.append(nodoCopiar);
+    });
+  }
+}
 
-for (const nodo of listaNodos) {
-  nodo.addEventListener("click", () => {
-    console.log(nodo);
-    const nodoCopiar = nodo.cloneNode(true);
-    const resultados = document.querySelector(".resultado");
-    resultados.append(nodoCopiar);
-  });
+const mayusculas = palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1);
+letrasMayus(mayusculas);
+function letrasMayus(cb) {
+  listaNodos[0].textContent = cb(listaNodos[0].textContent);
 }
