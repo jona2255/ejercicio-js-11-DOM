ad75ad15849a3962fd90aed3c1cf36619b703a1d
Jonatan Sánchez <jona2255@gmail.com>
2021-03-02 18:57:38 +0100

    Añadidos eventListener al input Text, al botón y controla el número de veces que se tiene que copiar

--- js/index.js
+++ js/index.js
@@ -1,10 +1,40 @@
-const listaNodos = document.querySelectorAll(".palabra");
+let listaNodos = document.querySelectorAll(".palabra");
+const listaPalabras = document.querySelector(".lista-palabras");
+const textoForm = document.querySelector("input[type='text']");
+const botonCrear = document.querySelector("button");
+const menuDesplegable = document.querySelector("select");
 
-for (const nodo of listaNodos) {
-  nodo.addEventListener("click", () => {
-    console.log(nodo);
-    const nodoCopiar = nodo.cloneNode(true);
-    const resultados = document.querySelector(".resultado");
-    resultados.append(nodoCopiar);
-  });
+let palabraTextForm = "";
+
+textoForm.addEventListener("change", () => {
+  botonCrear.disabled = false;
+  palabraTextForm = textoForm.value;
+});
+botonCrear.addEventListener("click", (event) => {
+  const palabraForm = document.createElement("li");
+  palabraForm.classList.add("palabra");
+  palabraForm.value = menuDesplegable.value;
+  palabraForm.textContent = palabraTextForm;
+  listaPalabras.append(palabraForm);
+  event.preventDefault();
+  listaNodos = document.querySelectorAll(".palabra");
+  copiarNodo();
+});
+function copiarNodo() {
+  for (const nodo of listaNodos) {
+    nodo.addEventListener("click", () => {
+      console.log(nodo);
+      if (nodo.value >= 0 || nodo.value === null) {
+        const nodoCopiar = nodo.cloneNode(true);
+        const resultados = document.querySelector(".resultado");
+        resultados.append(nodoCopiar);
+        if (nodo.value === 1) {
+          nodo.value = -1;
+        } else if (nodo.value > 1) {
+          nodo.value--;
+        }
+      }
+    });
+  }
 }
+copiarNodo();
