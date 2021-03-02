a3734354b329dc69b35e68fda52497a5eb9d8995
Jonatan Sánchez <jona2255@gmail.com>
2021-03-02 21:16:41 +0100

    Implementado el filtrado de la palabra

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ const listaPalabras = document.querySelector(".lista-palabras");
 const textoForm = document.querySelector("input[type='text']");
 const botonCrear = document.querySelector("button");
 const menuDesplegable = document.querySelector("select");
+const resultados = document.querySelector(".resultado");
 
 let palabraTextForm = "";
-
 textoForm.addEventListener("change", () => {
   if (textoForm.value.length > 0) {
     botonCrear.disabled = false;
@@ -15,14 +15,26 @@ textoForm.addEventListener("change", () => {
   }
 });
 botonCrear.addEventListener("click", (event) => {
-  const palabraForm = document.createElement("li");
-  palabraForm.classList.add("palabra");
-  palabraForm.value = menuDesplegable.value;
-  palabraForm.textContent = palabraTextForm;
-  listaPalabras.append(palabraForm);
-  event.preventDefault();
-  listaNodos = document.querySelectorAll(".palabra");
-  copiarNodo();
+  let palabraRepetida;
+
+  for (const nodo of listaNodos) {
+    if (nodo.textContent === palabraTextForm) {
+      palabraRepetida = true;
+    }
+  }
+  if (palabraRepetida || palabraTextForm.includes(" ")) {
+    event.preventDefault();
+    alert("La palabra está repetida o no es válida, escribe otra por favor");
+  } else {
+    const palabraForm = document.createElement("li");
+    palabraForm.classList.add("palabra");
+    palabraForm.value = menuDesplegable.value;
+    palabraForm.textContent = palabraTextForm;
+    listaPalabras.append(palabraForm);
+    event.preventDefault();
+    listaNodos = document.querySelectorAll(".palabra");
+    copiarNodo();
+  }
 });
 function copiarNodo() {
   for (const nodo of listaNodos) {
@@ -30,7 +42,6 @@ function copiarNodo() {
       console.log(nodo);
       if (nodo.value >= 0 || nodo.value === null) {
         const nodoCopiar = nodo.cloneNode(true);
-        const resultados = document.querySelector(".resultado");
         resultados.append(nodoCopiar);
         if (nodo.value === 1) {
           nodo.value = -1;
