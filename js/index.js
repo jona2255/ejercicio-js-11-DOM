ed55d5aabd00bc356fae5f9135ff8388da5f1ba6
Samuel MPD <samuelhoward1993@gmail.com>
2021-03-02 22:29:57 +0100

    borrapalabras y anyadidas clases etiquetas span

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
 const listaNodos = document.querySelectorAll(".palabra");
+const listaDerecha = document.querySelector(".resultado");
 
 for (const nodo of listaNodos) {
   nodo.addEventListener("click", () => {
     console.log(nodo);
     const nodoCopiar = nodo.cloneNode(true);
-    const resultados = document.querySelector(".resultado");
-    resultados.append(nodoCopiar);
+    listaDerecha.append(nodoCopiar);
   });
 }
+
+for (const nodo of listaDerecha) {
+  nodo.addEventListener("click", () => {
+    console.log(nodo);
+    const nodoBorrar = nodo.removeNode(true);
+    nodo.append(nodoBorrar);
+  });
+}
+
+const numPalabras = document.querySelector(".numero-palabras");
+const numCaracteres = document.querySelector(".numero-caracteres");
+const mediaPalabras = document.querySelector(".media-palabras");
